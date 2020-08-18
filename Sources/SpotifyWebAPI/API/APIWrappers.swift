c3c16b10c8aa84c1e63a0bad3e1e540cf0ac60bd
Peter-Schorn <petervschorn@gmail.com>
2020-08-18 02:12:14 -0500

    Finished refactoring get and post requests.

--- Sources/SpotifyWebAPI/API/APIWrappers.swift
+++ Sources/SpotifyWebAPI/API/APIWrappers.swift
@@ -11,27 +11,19 @@ extension SpotifyAPI {
         responseType: ResponseType.Type
     ) -> AnyPublisher<ResponseType, Error> {
 
-        let endpoint = Endpoints.apiEndpoint(
-            path, queryItems: removeIfNil(queryItems)
+        func makeHeaders(_ accessToken: String) -> [String: String] {
+            return Headers.bearerAuthorization(accessToken)
+        }
+        
+        return apiRequest(
+            path: path,
+            queryItems: queryItems,
+            requiredScopes: requiredScopes,
+            makeHeaders: makeHeaders,
+            httpMethod: "GET",
+            body: nil as Data?,
+            responseType: ResponseType.self
         )
-        
-        self.logger.trace("endpoint: '\(endpoint)'")
-        
-        return self.refreshAccessToken()
-            .tryMap { _ in
-                return try self.ensureAuthorized(
-                    forScopes: requiredScopes
-                ).accessToken
-            }
-            .flatMap { accessToken in
-                URLSession.shared.dataTaskPublisher(
-                    url: endpoint,
-                    httpMethod: "GET",
-                    headers: Headers.bearerAuthorization(accessToken)
-                )
-                .spotifyDecode(ResponseType.self)
-            }
-            .eraseToAnyPublisher()
         
     }
     
@@ -43,55 +35,39 @@ extension SpotifyAPI {
         body: Body,
         responseType: ResponseType.Type
     ) -> AnyPublisher<ResponseType, Error> {
-        do {
-            
-            let encodedBody = try JSONEncoder().encode(body)
-            
-            let endpoint = Endpoints.apiEndpoint(
-                path, queryItems: removeIfNil(queryItems)
-            )
-            
-            self.logger.trace("endpoint: '\(endpoint)'")
-            
-            return self.refreshAccessToken()
-                .tryMap { _ in
-                    return try self.ensureAuthorized(
-                        forScopes: requiredScopes
-                    ).accessToken
-                }
-                .flatMap { accessToken -> AnyPublisher<ResponseType, Error> in
-                    
-                    let headers =
-                        Headers.bearerAuthorization(accessToken) +
-                        Headers.acceptApplicationJSON
-                    
-                    return URLSession.shared.dataTaskPublisher(
-                        url: endpoint,
-                        httpMethod: "POST",
-                        headers: headers,
-                        body: encodedBody
-                    )
-                    .spotifyDecode(ResponseType.self)
-                    
-                }
-                .eraseToAnyPublisher()
-            
-        } catch {
-            return error.anyFailingPublisher(ResponseType.self)
+
+        
+        func makeHeaders(_ accessToken: String) -> [String: String] {
+            return Headers.bearerAuthorization(accessToken) +
+            Headers.acceptApplicationJSON
         }
+        
+        return apiRequest(
+            path: path,
+            queryItems: queryItems,
+            requiredScopes: requiredScopes,
+            makeHeaders: makeHeaders,
+            httpMethod: "POST",
+            body: body,
+            responseType: ResponseType.self
+        )
         
     }
     
 
+    /// All requests to endpoints other than those
+    /// for authorizing the app and retrieving/refreshing the tokens
+    /// call through to this method.
     func apiRequest<Body: Encodable, ResponseType: Decodable>(
         path: String,
         queryItems: [String : LosslessStringConvertible?],
         requiredScopes: Set<Scope>,
-        headers: [String: String],
+        makeHeaders: @escaping (_ accessToken: String) -> [String: String],
         httpMethod: String,
         body: Body?,
         responseType: ResponseType.Type
     ) -> AnyPublisher<ResponseType, Error> {
+        
         do {
             
             let encodedBody = try body.map {
@@ -114,7 +90,7 @@ extension SpotifyAPI {
                     return URLSession.shared.dataTaskPublisher(
                         url: endpoint,
                         httpMethod: httpMethod,
-                        headers: headers,
+                        headers: makeHeaders(accessToken),
                         body: encodedBody
                     )
                     .spotifyDecode(ResponseType.self)
@@ -122,8 +98,6 @@ extension SpotifyAPI {
                 }
                 .eraseToAnyPublisher()
                 
-            
-            
         } catch {
             return error.anyFailingPublisher(ResponseType.self)
         }
--- Sources/SpotifyWebAPI/Other/Headers.swift
+++ Sources/SpotifyWebAPI/Other/Headers.swift
@@ -56,6 +56,4 @@ enum Headers {
         
     }
     
-    
-    
 }
