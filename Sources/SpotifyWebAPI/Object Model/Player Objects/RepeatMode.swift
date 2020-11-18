8a591f7146ac5778eee8bf741254f6184595f81b
Peter-Schorn <petervschorn@gmail.com>
2020-11-18 14:20:05 -0600

    Removed incorrect documentation for RepeatMode

--- Sources/SpotifyWebAPI/Object Model/Player Objects/RepeatMode.swift
+++ Sources/SpotifyWebAPI/Object Model/Player Objects/RepeatMode.swift
@@ -10,15 +10,7 @@ public enum RepeatMode: String, Codable, Hashable, CaseIterable {
     /// The current track is playing on repeat.
     case track
     
-    /**
-     The current context, such as a playlist, is playing on repeat.
-     
-     Indicates that when the user reaches the end of the context,
-     the track/episode at the beginning of the context will play again,
-     and so on. If repeat mode is off, then, depending on the user's
-     settings, playback may stop after the user reaches the end of the
-     context or songs similar to the context may begin to play.
-     */
+    /// The current context, such as a playlist, is playing on repeat.
     case context
     
 }
