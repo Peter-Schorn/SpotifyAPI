44e1a7b23eef0424be46d2b8dcb5c39afa5ab323
Peter-Schorn <petervschorn@gmail.com>
2021-01-10 07:12:13 -0600

    Small documentation change.

--- Sources/SpotifyWebAPI/Authorization/AuthorizationCodeFlowPKCEManager.swift
+++ Sources/SpotifyWebAPI/Authorization/AuthorizationCodeFlowPKCEManager.swift
@@ -142,7 +142,7 @@ public final class AuthorizationCodeFlowPKCEManager:
      
      **In general, only use this initializer if you have retrieved the**
      **authorization information from an external source.** Otherwise, use
-     `init(clientId:clientSecret:)`.
+     ``init(clientId:clientSecret:networkAdaptor:)``.
     
      You are discouraged from individually saving the properties of this instance
      to persistent storage and then retrieving them later and passing them into
