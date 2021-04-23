e76f33d215c8c48d08d0009155baa3adf60b740f
Peter-Schorn <petervschorn@gmail.com>
2021-04-23 16:20:48 -0500

    changed !TEST to TEST

--- Sources/SpotifyAPITestUtilities/NetworkAdaptor.swift
+++ Sources/SpotifyAPITestUtilities/NetworkAdaptor.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-#if !TEST
+#if TEST
 import NIO
 import NIOHTTP1
 import AsyncHTTPClient
@@ -22,14 +22,14 @@ public final class NetworkAdaptorManager {
     
     public static let shared = NetworkAdaptorManager()
     
-    #if !TEST
+    #if TEST
     private let httpClient = HTTPClient(eventLoopGroupProvider: .createNew)
     #endif
     
     private init() { }
     
     deinit {
-        #if !TEST
+        #if TEST
         try? self.httpClient.syncShutdown()
         #endif
     }
@@ -38,7 +38,7 @@ public final class NetworkAdaptorManager {
         request: URLRequest
     ) -> AnyPublisher<(data: Data, response: HTTPURLResponse), Error> {
         
-        #if !TEST
+        #if TEST
         // transform the dictionary to an array of tuples
         let headers: [(String, String)] = (request.allHTTPHeaderFields ?? [:])
             .map { key, value in return (key, value) }
